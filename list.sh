4fbff414b2d3d6fc8e0f1f8fa898c9044f98b3ae
zhouquan <quillzhou@gmail.com>
2019-02-27 11:57:41 +0800

    ✨add vscode folder

--- list.sh
+++ list.sh
@@ -1,1 +1,7 @@
-code --list-extensions | xargs -L 1 echo code --install-extension > vscode.extension.sh
+# macOS only
+# Generate vscode.extension.sh, make sure you have installed code command
+code --list-extensions | xargs -L 1 echo code --install-extension > vscode/extension.sh
+
+# copy vscode config
+# vscsetting="~/Library/Application\ Support/Code/User"
+cp -r ~/Library/Application\ Support/Code/User/{settings.json,keybindings.json,snippets} vscode
