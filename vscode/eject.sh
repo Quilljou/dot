c7c862cbbdfc57a213236048c4a9396e00140bb8
周全 <zhouquan10@ECCMAC-C02VP12N.local>
2019-07-10 10:21:05 +0800

    vscode restore in osx

--- vscode/eject.sh
+++ vscode/eject.sh
@@ -4,11 +4,13 @@
 
 if [ "$(uname)" = "Darwin" ]
 then
-    echo "mac"
+    cp -r $(pwd)/vscode/{settings.json,keybindings.json,snippets} ~/Library/Application\ Support/Code/User
+    echo "vscode data restored on OSX"
 elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]
 then
     echo "linux"
 elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW64_NT" ]
 then
     cp -r $(pwd)/vscode/{settings.json,keybindings.json,snippets} ~/Application\ Data/Code/User
+    echo "vscode data restored on Windows"
 fi
