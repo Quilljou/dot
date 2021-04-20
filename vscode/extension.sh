a12aafdea5870df325535d7adaeacde102cdba10
quillzhou <zhouquan10@jd.com>
2021-04-20 11:21:37 +0800

    feat: backup

--- vscode/extension.sh
+++ vscode/extension.sh
@@ -1,4 +1,3 @@
-code --install-extension 2gua.rainbow-brackets
 code --install-extension AlanWalk.markdown-toc
 code --install-extension ElemeFE.vscode-element-helper
 code --install-extension auchenberg.vscode-browser-preview
@@ -10,6 +9,9 @@ code --install-extension bierner.markdown-mermaid
 code --install-extension bierner.markdown-preview-github-styles
 code --install-extension bierner.markdown-yaml-preamble
 code --install-extension christian-kohler.npm-intellisense
+code --install-extension Chukong Technologies.cocos-creator
+code --install-extension Chukong Technology.cocos-debug
+code --install-extension csstools.postcss
 code --install-extension cstuder.gitlab-ci-validator
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension drKnoxy.eslint-disable-snippets
@@ -17,6 +19,7 @@ code --install-extension eamodio.gitlens
 code --install-extension ecmel.vscode-html-css
 code --install-extension EditorConfig.EditorConfig
 code --install-extension eg2.vscode-npm-script
+code --install-extension egretengine.coder
 code --install-extension FallenMax.mithril-emmet
 code --install-extension felixfbecker.php-debug
 code --install-extension felixfbecker.php-intellisense
@@ -26,15 +29,16 @@ code --install-extension formulahendry.code-runner
 code --install-extension hedinne.stylelint-disable-snippets
 code --install-extension HookyQR.beautify
 code --install-extension jasonHzq.vscode-pont
+code --install-extension jebbs.plantuml
 code --install-extension jeremyrajan.webpack
 code --install-extension kameshkotwani.google-search
 code --install-extension liximomo.sftp
 code --install-extension mhutchie.git-graph
 code --install-extension mrmlnc.vscode-duplicate
 code --install-extension ms-azuretools.vscode-azureappservice
+code --install-extension ms-azuretools.vscode-azureresourcegroups
 code --install-extension ms-vscode.atom-keybindings
 code --install-extension ms-vscode.azure-account
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension naumovs.color-highlight
@@ -42,7 +46,6 @@ code --install-extension octref.vetur
 code --install-extension Orta.vscode-jest
 code --install-extension qiu8310.minapp-vscode
 code --install-extension QuanZhou.command-runner
-code --install-extension rbbit.typescript-hero
 code --install-extension rebornix.project-snippets
 code --install-extension redhat.vscode-yaml
 code --install-extension ritwickdey.LiveServer
@@ -56,5 +59,5 @@ code --install-extension techer.open-in-browser
 code --install-extension WASTeamAccount.WebTemplateStudio-dev-nightly
 code --install-extension wayou.vscode-todo-highlight
 code --install-extension wix.vscode-import-cost
-code --install-extension zhuangtongfa.Material-theme
+code --install-extension zhuangtongfa.material-theme
 code --install-extension Zignd.html-css-class-completion
